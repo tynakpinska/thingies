0f68370e52222b7dc7b95661016b6866cd982020
tynakpinska <m.k.kepinska@gmail.com>
2023-12-16 14:49:41 +0100

    change api

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,13 +8,13 @@ const Card = ({ product }) => {
       style={{ backgroundColor: "#FFAAAA" }}
     >
       <img
-        src={product.image}
+        src={product.images[0]}
         className="card-img-top img-fluid p-2"
         alt="..."
         style={{ backgroundColor: "#FFF" }}
       />
       <div className="card-body p-1 d-flex flex-column justify-content-center align-items-center flex-grow-0">
-        <p className="card-title">{product.title.slice(0, 15) + "..."}</p>
+        <p className="card-title">{product.title}</p>
         <p>{product.price + "$"}</p>
         <div className="d-flex align-items-center justify-items-center mt-1">
           {heart}
--- src/redux/stateSlices/productsSlice.js
+++ src/redux/stateSlices/productsSlice.js
@@ -3,17 +3,20 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const shopApi = createApi({
   reducerPath: "shopApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://fakestoreapi.com/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.escuelajs.co/api/v1" }),
   endpoints: builder => ({
     getProducts: builder.query({
-      query: () => "products/",
+      query: () => "products",
     }),
     getRandomProduct: builder.query({
       query: productNumber => `products/${productNumber}`,
     }),
+    getCategories: builder.query({
+        query: () => "categories",
+      }),
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetProductsQuery, useGetRandomProductQuery } = shopApi;
+export const { useGetProductsQuery, useGetRandomProductQuery, useGetCategoriesQuery } = shopApi;
